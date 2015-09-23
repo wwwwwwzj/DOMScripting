a993cc769bc4db075c6bac4cae2396cff86b24e1
Zhenjiang Wang <wangzj_0826@163.com>
2015-09-23 22:45:13 +0800

    update chapter12
    update photos and about pages

--- chapter12/scripts/global.js
+++ chapter12/scripts/global.js
@@ -132,5 +132,84 @@ function prepareSlideshow() {
 		}
 	} 
 }
+function showSection(id) {
+	var sections = document.getElementsByTagName("section");
+	for (var i = 0; i < sections.length; i++) {
+		if (sections[i].getAttribute("id") != id) {
+			sections[i].style.display = "none";
+		} else {
+			sections[i].style.display = "block";
+		}
+	}
+}
+function prepareInternalnav() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	var articles = document.getElementsByTagName("article");
+	if (articles.length == 0) return false;
+	var navs = articles[0].getElementsByTagName("nav");
+	if (navs.length == 0) return false;
+	var nav = navs[0];
+	var links = nav.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		var sectionId = links[i].getAttribute("href").split("#")[1];
+		if (!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display = "none";
+		links[i].destination = sectionId;
+		links[i].onclick = function() {
+			showSection(this.destination);
+			return false;
+		}
+	}
+}
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return true;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id","placeholder");
+  placeholder.setAttribute("src","images/placeholder.gif");
+  placeholder.setAttribute("alt","my image gallery");
+  var description = document.createElement("p");
+  description.setAttribute("id","description");
+  var desctext = document.createTextNode("Choose an image");
+  description.appendChild(desctext);
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(description,gallery);
+  insertAfter(placeholder,description);
+}
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for ( var i=0; i < links.length; i++) {
+    links[i].onclick = function() {
+      return showPic(this);
+	}
+  }
+}
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
